47d78d43611a0e0d7a8e8844fcce20a4c61fbf4c
Lawrence Kati <katilaw@gmail.com>
2016-02-15 10:56:21 -0500

    modified code

--- local_events.rb
+++ local_events.rb
@@ -2,9 +2,19 @@ require 'net/http'
 require 'pry'
 require 'json'
 
-uri = URI("http://api.seatgeek.com/2/events?venue.city="+ARGV[0])
-parse = JSON.parse(Net::HTTP.get(uri))
+my_city = "Boston"
+my_state = "MA"
+today = Time.now.strftime("%Y-%m-%d")
 
-parse["events"].each do |event|
-  puts event["title"] + " @ " + event["venue"]["name"]
+url = "https://api.seatgeek.com/2/events"
+url += "?venue.city=#{my_city}"
+url += "&venue.state=#{my_state}"
+url += "&datetime_local.gte=#{today}"
+url += "&datetime_local.gte=#{today}"
+
+uri = URI(url)
+res = Net::HTTP.get_response(uri)
+events = JSON.parse(res.body)["events"]
+events.each do |event|
+  puts "#{event["title"]} @ #{event["venue"]["name"]}"
 end
